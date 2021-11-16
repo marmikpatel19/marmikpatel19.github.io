16b0519e3ca0589ce1370d3891d23300097aa6ac
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2021-11-16 14:45:39 -0700

    style: update header location, size, and close toggle

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ selectElement('.hamburger').addEventListener('click', () => {
 	selectElement('.hamburger').classList.toggle('active');
 	selectElement('.nav__list').classList.toggle('active');
 })
+
+// Close menu when a link is clicked
+selectElement('.nav__list').addEventListener('click', () => {
+	selectElement('.hamburger').classList.toggle('active');
+	selectElement('.nav__list').classList.toggle('active');
+})
