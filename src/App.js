b717775b0b45a85b8a88ac90157f724f25f583a0
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-01-20 20:36:44 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import Home from "./components/pages/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/*
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experiences" element={<Experiences />} />
+          <Route path="/management" element={<Management />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
