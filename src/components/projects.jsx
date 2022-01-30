09c6c024e13cafae32b06faac2a40bfa57f820fe
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-01-30 11:27:33 -0700

    update projects content

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Project from "./project";
+import "../styling/projects.css";
 
 function Projects() {
   return (
@@ -8,27 +9,27 @@ function Projects() {
 
       <div className="projects-container">
         <Project
-          src="images/-.jpg"
-          alt="-"
-          title=""
-          discription=""
+          src="images/Student-Residence-System.png"
+          alt="Image of Marmik's Student Residence System project"
+          title="Student Residence System"
+          discription="An object-oriented university residence system developed using Java. It was constructed through six application versions with various software design patterns and includes a GUI for an administrator to personalize the system."
           link=""
           sideLeft="false"
         />
         <Project
-          src="images/-.jpg"
-          alt="-"
-          title=""
-          discription=""
-          link=""
+          src="images/EnviroSearch-one.png"
+          alt="Image of Marmik's EnviroSearch project"
+          title="EnviroSearch"
+          discription="An android app that displays corporations’ environmental footprints. Developed on Android Studio/Java using Google Custom Search JSON API by making RESTful requests through the .NET and JSON libraries. It was a group project for Hack the Valley V."
+          link="https://github.com/marmikpatel19/EnviroSearch"
           sideLeft="true"
         />
         <Project
-          src="images/-.jpg"
-          alt="-"
-          title=""
-          discription=""
-          link=""
+          src="images/BookBees-one.png"
+          alt="Image of Marmik's BookBees project"
+          title="BookBees"
+          discription="A single-page beekeeping application developed using React.js, HTML, and CSS to provide local businesses with an interactive, responsive UI."
+          link="https://github.com/marmikpatel19/BookBees"
           sideLeft="false"
         />
       </div>
