217c94aa25a36742d620811fee5a46d8dd36c87b
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-01-31 16:31:15 -0700

    add second link to project

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -5,13 +5,18 @@ function Project(props) {
     css styling depending on the class */
 
   return (
-    <div className="project">
+    <div className="project" id={props.id}>
       <img className="project-photo" src={props.src} alt={props.alt} />
-      <h1 className="project-title">{props.title}</h1>
-      <h3 className="project-discription">{props.discription}</h3>
-      <a target="_blank" href={props.link}>
-        <i class="fab fa-github"></i>
-      </a>
+      <div className="text">
+        <h1 className="project-title">{props.title}</h1>
+        <h3 className="project-discription">{props.discription}</h3>
+        <a target="_blank" href={props.gitlink} id="link1">
+          <i class="fab fa-github"></i>
+        </a>
+        <a target="_blank" href={props.link} id="link2">
+          <i class="fas fa-external-link-alt"></i>
+        </a>
+      </div>
     </div>
   );
 }
