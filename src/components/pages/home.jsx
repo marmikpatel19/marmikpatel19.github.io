1325f29f10702c328412b6c6d6dcd22b8af15e70
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-02-03 09:16:01 -0700

    add projects to home page

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../../App.css";
 import Navbar from "../navbar";
 import Aboutme from "../aboutme";
+import Projects from "../projects";
 
 function home() {
   return (
     <>
       <Navbar />
       <Aboutme />
+      <Projects />
     </>
   );
 }
