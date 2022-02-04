03c33e59f69514b0151e5d4da964a1d18de4b561
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-02-03 19:52:05 -0700

    create experiences component

---
+++ src/components/experiences.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Experience from "./expeirence";
+
+function Experiences() {
+  return (
+    <div className="experiences">
+      <h1 className="section-title">Experiences & Education</h1>
+
+      <Experience
+        title="Robotics Course Developer and Instructor"
+        company="@ Infokidz Academy"
+        time="2017"
+        bullet1="Architected a robotics course using a visual programming language and robots, structuring 23 activities."
+        bullet2="Taught six students by practicing strong communication and analytical skills, receiving outstanding student-parent satisfaction."
+      />
+    </div>
+  );
+}
+
+export default Experiences;
