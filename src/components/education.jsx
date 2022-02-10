fed11eb7e4ac898d67f25efbed538cd921c2ce7b
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-02-09 22:16:32 -0600

    add education component

---
+++ src/components/education.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styling/experiences.css";
+import "../styling/education.css";
+
+function Education() {
+  return (
+    <>
+      <hr></hr>
+
+      <div className="education">
+        {/*Education One*/}
+        <h1 className="university">University of Saskatchewan</h1>
+        <h2 className="timeline">Sep. 2021 - Present</h2>
+        <h2 className="degree">Bachelor of Science, Computer Science</h2>
+
+        {/*Education Two*/}
+        <h1 className="university">University of Toronto</h1>
+        <h2 className="timeline">Sep. 2020 - Aug. 2021</h2>
+        <h2 className="degree">
+          Bachelor of Science, Computer Science, Mathematics, Statistics
+          (incomplete)
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default Education;
