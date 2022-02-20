da52c54c7d092202cbe7cd195414dab7112eeef2
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-02-20 11:02:20 -0700

    update management expeirences content

--- src/components/managementExps.jsx
+++ src/components/managementExps.jsx
@@ -4,7 +4,7 @@ import "../styling/management.css";
 
 function ManagementExps() {
   return (
-    <div className="management">
+    <div className="management-experiences">
       <h1 className="section-title">Management & Environmentalism</h1>
 
       {/*Container for compartments of two experiences */}
@@ -12,28 +12,26 @@ function ManagementExps() {
         <Management
           title="Founding President @ PlantForever"
           time="Aug 2017 - Present"
-          discription="I manage the environmental nonprofit PlantForever, driving the vision to develop urban forests through tree planting to mitigate the climate crisis. I collaborate with a four-person executive team and 10-30 annual volunteers. I have led PlantForever to (in)directly plant 10,000+ trees and raise awareness to over 1.7 million individuals."
-          src="images/--"
-          alt="--"
+          discription="I manage the nonprofit PlantForever, driving the vision to develop urban forests. I collaborate with a four-person team and 10-30 volunteers, leading PlantForever to (in)directly plant 10,000+ trees and engage 1,700,000+ individuals. "
+          src="images/PlantForever-One.jpg"
+          alt="Image of Marmik's PlantForever management experience"
           link1="https://www.plantforever.org/"
           link2="https://www.plantforever.org/in-the-news"
-          linke2Logo="~fontawesome logo~"
+          link2LogoIndex="0"
           id="PlantForever"
         />
-        <Management />
 
         <Management
-          title="Marketing Executive @ Young Climate Leaders (YCL)"
+          title="Marketing Executive @ Young Climate Leaders"
           time="Feb 2020 - May 2021"
-          discription="I led the marketing team at YCL, an organization that hosts environmental conferences. I developed social media, ad, and school-oriented marketing campaigns, capturing 75 attendees. Additionally, I delivered a 40 minute presentation to a 45-person international audience."
-          src="images/--"
-          alt="--"
+          discription="I led the marketing team at YCL, an organization that hosts climate conferences. Developing various marketing campaigns, I captured 75 attendees. Additionally, I delivered a presentation to a 45-person audience."
+          src="images/YCL-One.png"
+          alt="Image of Marmik's Young Climate Leaders management experience"
           link1="https://www.youngclimateleaders.org/"
           link2="https://www.youngclimateleaders.org/speakers-1?pgid=kxtwryza-023fa57a-babd-4c29-b327-7173bb9aa76b"
-          linke2Logo="~fontawesome logo~"
+          link2LogoIndex="1"
           id="YCL"
         />
-        <Management />
       </div>
     </div>
   );
