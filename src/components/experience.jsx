d668d0afce20e878fdc5e391244214584b06fd2e
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2023-06-04 23:30:30 -0600

    remove all unnecessary code

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,28 +1,5 @@
-import React from "react";
-
 function Experience(props) {
-  return (
-    <div className="experience">
-      <h1 className="experience-title">{props.title}</h1>
-      <h3 className="experience-time">{props.time}</h3>
-      <ul>
-        <li>
-          <h3 className="experience-bullets">
-            {props.bullet1}{" "}
-            <a
-              href="https://github.com/Infokidz-Academy/Infokidz-Academy-Website"
-              target="_blank"
-            >
-              here
-            </a>
-          </h3>
-        </li>
-        <li>
-          <h3 className="experience-bullets">{props.bullet2}</h3>
-        </li>
-      </ul>
-    </div>
-  );
+  return <></>;
 }
 
 export default Experience;
