1f5f27bc3867166b46e9a4ad7ef0665fcda5ac3f
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2023-06-04 23:59:17 -0600

    set default font family and color palette

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import "./App.css";
 import Home from "./components/pages/home";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Poppins"].join(","),
+  },
+  palette: {
+    primary: {
+      main: "#bde8f1",
+    },
+    background: {
+      main: "#f2f2f0",
+    },
+    dark: {
+      main: "#0d0d0d",
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </ThemeProvider>
   );
 }
 
