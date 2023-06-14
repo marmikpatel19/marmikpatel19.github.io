860b49ae1f30ba1bbd55daf79b168fb805367859
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2023-06-13 19:32:07 -0600

    add icons/links

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material";
+import { Box, Chip, Link, Typography } from "@mui/material";
 import { Container } from "@mui/system";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function Hero() {
   return (
@@ -47,6 +49,57 @@ function Hero() {
       >
         Computer Science student
       </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row-reverse",
+          marginTop: "2%",
+        }}
+      >
+        <Link href="https://github.com/marmikpatel19" color="inherit">
+          <GitHubIcon
+            sx={{
+              marginTop: "-5px",
+              fontSize: {
+                xs: "27px",
+                sm: "36px",
+              },
+            }}
+          />
+        </Link>
+        <Link href="https://linkedin.com/in/marmikp" color="inherit">
+          <LinkedInIcon
+            sx={{
+              marginRight: "5px",
+              marginTop: "-5px",
+              fontSize: {
+                xs: "30px",
+                sm: "40px",
+              },
+            }}
+          />
+        </Link>
+        <Chip
+          label="Resume"
+          variant="outlined"
+          component="a"
+          href="https://marmikpatel19.github.io/MarmikPatelResume.pdf"
+          clickable
+          sx={{
+            borderRadius: "10%",
+            marginRight: "7px",
+            fontWeight: "bold",
+            height: {
+              xs: "23px",
+              sm: "32px",
+            },
+            // fontSize: {
+            //   xs: "10px",
+            //   sm: "h7.fontSize",
+            // },
+          }}
+        />
+      </Box>
     </Container>
   );
 }
